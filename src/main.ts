f80796e6b4c669ea4523d1aa68e034045471c92b
Luciano Martins <luciano.martinsjr@hotmail.com>
2023-11-07 23:00:52 -0300

    ajuste env e porta

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appConfig } from './config/app.config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  await app.listen(appConfig().port);
 }
 bootstrap();
+
